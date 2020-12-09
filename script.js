eaf0b89ff586cf03eec60dc458185c82a9b20915
Andrea  Hinkle <andreahinkle@Andreas-MacBook-Pro-2.local>
2020-12-09 00:26:23 -0500

    added content

--- script.js
+++ script.js
@@ -8,11 +8,24 @@ var sail_char = document.getElementById("sail-annie");
 var winter_char = document.getElementById("winter-annie");
 var surfer = document.getElementById("surfer");
 var boat = document.getElementById("boat");
+
+var content = document.getElementById("directions");
+var content27 = document.getElementById("content-27");
+var content28 = document.getElementById("content-28");
+var content29 = document.getElementById("content-29");
+var content30 = document.getElementById("content-30");
+var content31 = document.getElementById("content-31");
+var content32 = document.getElementById("content-32");
+var content33 = document.getElementById("content-33");
+var content34 = document.getElementById("content-34");
+var content35 = document.getElementById("content-35");
+
 function walk() {
 
+     /* character evolution */
     if (bg.scrollTop < 4100) {
         boat.style.visibility = "visible";
-        boat.style.left = "73%";
+        boat.style.left = "71.5%";
 
         character.style.visibility = "visible";
         space_char.style.visibility = "hidden";
@@ -21,9 +34,9 @@ function walk() {
         winter_char.style.visibility = "hidden";
         surfer.style.visibility = "hidden";
     }
-    else if (bg.scrollTop >= 11400 && bg.scrollTop <= 13000){
+    else if (bg.scrollTop >= 11400 && bg.scrollTop <= 12700){
         boat.style.visibility = "visible";
-        boat.style.left = "73%";
+        boat.style.left = "71.5%";
 
         character.style.visibility = "hidden";
         space_char.style.visibility = "hidden";
@@ -43,9 +56,9 @@ function walk() {
         winter_char.style.visibility = "visible";
         surfer.style.visibility = "hidden";
     }
-    else if (bg.scrollTop > 13000) {
+    else if (bg.scrollTop > 12700) {
         boat.style.visibility = "hidden";
-        boat.style.left = "73%";
+        boat.style.left = "71.5%";
 
         character.style.visibility = "hidden";
         space_char.style.visibility = "hidden";
@@ -56,7 +69,7 @@ function walk() {
     }
     else if (bg.scrollTop > 9000) {
         boat.style.visibility = "visible";
-        boat.style.left = "73%";
+        boat.style.left = "71.5%";
 
         character.style.visibility = "hidden";
         space_char.style.visibility = "hidden";
@@ -66,8 +79,9 @@ function walk() {
         surfer.style.visibility = "hidden";
     }
     else if (bg.scrollTop > 4100) {
+
         boat.style.visibility = "visible";
-        boat.style.left = "73%";
+        boat.style.left = "71.5%";
         
         character.style.visibility = "hidden";
         space_char.style.visibility = "visible";
@@ -76,6 +90,72 @@ function walk() {
         winter_char.style.visibility = "hidden";
         surfer.style.visibility = "hidden";
     }
+
+    /* content blocks */
+    if (bg.scrollTop < 500) {
+        content.style.visibility = "visible";
+    }
+    if (bg.scrollTop > 500) {
+        content.style.visibility = "hidden";
+    }
+    if (bg.scrollTop < 4100){
+        content27.style.visibility = "hidden";
+        content28.style.visibility = "hidden";
+        content29.style.visibility = "hidden";
+        content30.style.visibility = "hidden";
+        content31.style.visibility = "hidden";
+        content32.style.visibility = "hidden";
+        content33.style.visibility = "hidden";
+        content34.style.visibility = "hidden";
+        content35.style.visibility = "hidden";
+        content27.classList.remove("animate_appear27");
+        content28.classList.remove("animate_appear28");
+        content29.classList.remove("animate_appear29");
+        content30.classList.remove("animate_appear30");
+        content31.classList.remove("animate_appear31");
+        content32.classList.remove("animate_appear32");
+        content33.classList.remove("animate_appear33");
+        content34.classList.remove("animate_appear34");
+        content35.classList.remove("animate_appear35");
+    }
+    if (bg.scrollTop > 5300) {
+        content32.style.visibility = "visible";
+        content32.classList.add("animate_appear32");
+    }
+    if (bg.scrollTop > 6500) {
+        content30.style.visibility = "visible";
+        content30.classList.add("animate_appear30");
+    }
+    if (bg.scrollTop > 7800) {
+        content31.style.visibility = "visible";
+        content31.classList.add("animate_appear31");
+    }
+    if (bg.scrollTop > 9300) {
+        content27.style.visibility = "visible";
+        content27.classList.add("animate_appear27");
+    }
+    if (bg.scrollTop > 9900) {
+        content28.style.visibility = "visible";
+        content28.classList.add("animate_appear28");
+    }
+    if (bg.scrollTop > 10600) {
+        content29.style.visibility = "visible";
+        content29.classList.add("animate_appear29");
+    }
+    if (bg.scrollTop > 13500) {
+        content33.style.visibility = "visible";
+        content33.classList.add("animate_appear33");
+    }
+    if (bg.scrollTop > 14500) {
+        content34.style.visibility = "visible";
+        content34.classList.add("animate_appear34");
+    }
+    if (bg.scrollTop > 15500) {
+        content35.style.visibility = "visible";
+        content35.classList.add("animate_appear35");
+    }
+
+
 
     if (prevScrollPos > bg.scrollTop) {
         if (character.classList != "animate_walk_bw"){ /* too many clicks */
